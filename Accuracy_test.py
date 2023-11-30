bc6649f14d1236db38b9c376092823e52bfb2912
Sander O'Neil <58371105+Sander-ONeil@users.noreply.github.com>
2023-11-30 14:51:54 -0500

    Add files via upload

--- Accuracy_test.py
+++ Accuracy_test.py
@@ -29,30 +29,68 @@ for r in Tdata[1:]:
         Bvector[i,Races[race]] = bad_Probs[race]
     i+=1
 
+import matplotlib.pyplot as plt
+import matplotlib.colors
+#import seaborn as sns
+
 races = ['White','Black','API','AIAN','Multi','Hisp','Other']
 
-def confusion(T,P):
-    t = np.argmax(T,axis = 1)
-    p = np.argmax(P,axis = 1)
-    c = np.zeros((2,2),dtype = int)
+N = 256
+vals = np.ones((N, 4))
+vals[:, 0] = np.linspace(90/256, 1, N)*0
+vals[:, 1] = np.linspace(90/256, 1, N)*0
+vals[:, 2] = np.linspace(90/256, 1, N)
+newcmp = matplotlib.colors.ListedColormap(vals)
+
+
+def confusion_plot(A, l, f):
+    f.imshow(A, interpolation='nearest', cmap=newcmp)
+    
+    f.set_xlabel('Predicted labels')
+    f.set_ylabel('True labels')
+
+    
+    f.set_xticks(range(A.shape[0]), l)
+    f.set_yticks(range(A.shape[1]), l)
     
     
+
+    # Loop over data dimensions and create text annotations.
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            text = f.text(j, i, A[i, j],
+                           ha="center", va="center", color="white",fontsize = 40)
+
     
-    for x,r in enumerate(races):
+    
+
+def confusion(T, P):
+    t = np.argmax(T, axis=1)
+    p = np.argmax(P, axis=1)
+
+    # Plot for each race
+    fig, axs = plt.subplots(1, 7, figsize=(20, 10))  # Adjust the layout as needed
+    axs = axs.flatten()
+    
+    for x, r in enumerate(races):
         pp = p == x
         pn = pp == False
         tp = t == x
         tn = tp == False
-        
-        TP = pp & tp
-        FP = pp & tn
-        TN = pn & tn
-        FN = pn & tp
-        
-        c[0] = [np.sum(TP),np.sum(FN)]
-        c[1] = [np.sum(FP),np.sum(TN)]
-        print(g_mas_order[x])
-        print(c)
+
+        TP = np.sum(pp & tp)
+        FP = np.sum(pp & tn)
+        TN = np.sum(pn & tn)
+        FN = np.sum(pn & tp)
+
+        c = np.array([[TP, FN], [FP, TN]])
+
+        confusion_plot(c,['True','False'],axs[x])
+        axs[x].set_title(r)
+    
+    
+    
+    plt.tight_layout()
     
     multiclass = np.zeros((7,7),dtype = int)
     
@@ -61,11 +99,16 @@ def confusion(T,P):
     for i in indices:
         multiclass[i[0],i[1]] += 1
     
-    print(multiclass)
+    fig, axs = plt.subplots(1, 1, figsize=(20, 10))
+    #axs = axs.flatten()
+    confusion_plot(multiclass,races,axs)
+    plt.title('Multi-class Confusion Matrix')
     
-    return c
+    plt.show()
 
 
+
+    
 distP = np.sum(abs(Pvector-tvector),axis = 1)
 distB = np.sum(abs(Bvector-tvector),axis = 1)
 distW = np.sum(abs(Worst-tvector),axis = 1)
